63b33f5af771302424cf3ad9d3819fa3adf197aa
GiordanDelaCruz <GiordanDelaCruz@gmail.com>
2020-02-21 19:08:14 -0500

    Added a sidebar to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/** OVERVIEW: This program is to provide functionality to the document objects
+*             within "Giordan's Online Math Notes".
+*/
+
+// Event Listener for the button contolling the side-navbar
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
+});
